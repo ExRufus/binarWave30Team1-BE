f9e1b93fd535a1ae1b01cedfb8ea2c14c4d3c971
rafikaauda <rafika.auda4@gmail.com>
2023-05-14 19:47:07 +0700

    add try catch on function getPlayer

--- Controllers/User.controller.js
+++ Controllers/User.controller.js
@@ -3,9 +3,19 @@ const prisma = new PrismaClient();
 
 // 1. fungsi create player / register - vincent
 // 2. fungsi get player - auda
-async function getPlayer (req, res) {
-    const players = await prisma.user.findMany();
-    res.json(players);
+async function getPlayer (req, res, next) {
+    try {
+        const players = await prisma.user.findMany();
+        if (players) {
+            return res.status(200).json({
+              result: "Success",
+              players,
+            });
+          }
+        
+    } catch (error) {
+        next(error);
+    }
   };
 // 3. fungsi get player berdasarkan id - adan
 // 4. fungsi update player - akmal
