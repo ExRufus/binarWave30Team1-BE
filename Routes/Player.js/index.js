32a17886ec408ea048801323e89fe1247de8ddd6
ExRufus <akmaldyn02@gmail.com>
2023-05-15 14:52:09 +0700

    Resolve merge conflict in User.controller.js and Player.js/index.js

--- Routes/Player.js/index.js
+++ Routes/Player.js/index.js
@@ -1,9 +1,9 @@
 const { getPlayer } = require("../../Controllers/User.controller");
+const UserController = require("../../Controllers/User.controller");
 const PlayerRouter = require("express").Router();
 
-
-PlayerRouter.get("/", getPlayer)
-
+PlayerRouter.get("/", getPlayer);
+PlayerRouter.delete("/:id", UserController.deletePlayer);
 PlayerRouter.put("/user/:id")
 
 module.exports = PlayerRouter;
