fa0988def93dea43163d88e5e7ee6bc31bfe281f
labibplop <lbabss9@gmail.com>
2023-05-15 19:56:01 +0700

    nyoba benerin bug delete labib

--- Routes/Player.js/index.js
+++ Routes/Player.js/index.js
@@ -1,13 +1,13 @@
 const {
   getPlayer,
   getPlayerById,
+  deletePlayer,
 } = require("../../Controllers/User.controller");
-const UserController = require("../../Controllers/User.controller");
 const PlayerRouter = require("express").Router();
 
 PlayerRouter.get("/", getPlayer);
 PlayerRouter.get("/:id", getPlayerById);
-PlayerRouter.delete("/:id", UserController.deletePlayer);
+PlayerRouter.delete("/:id", deletePlayer);
 PlayerRouter.put("/:id");
 
 module.exports = PlayerRouter;
