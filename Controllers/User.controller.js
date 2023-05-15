4d99c16053e9fd7ca7b28b2f9551f83a454cde5e
rafikaauda <rafika.auda4@gmail.com>
2023-05-15 17:56:46 +0700

    update getuser

--- Controllers/User.controller.js
+++ Controllers/User.controller.js
@@ -1,5 +1,4 @@
 const { PrismaClient } = require("@prisma/client");
-const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 // 1. fungsi create player / register - vincent
@@ -12,7 +11,13 @@ async function getPlayer(req, res, next) {
         result: "Success",
         players,
       });
+    } if (!players){
+      return res.status(400).json({
+        result: "error",
+        message: 'Tidak ada data',
+      })
     }
+  
   } catch (error) {
     next(error);
   }
@@ -59,7 +64,7 @@ async function updateUser(req, res, next) {
 }
 
 // 5, fungsi delete player - labib
-export const deletePlayer = async (req, res) => {
+async function deletePlayer(req, res, next) {
   try {
     const players = await prisma.user.delete({
       where: {
