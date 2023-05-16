1fb37ad8bd8b9588a663595fa127517897805680
Adanpradana <adanizkahariz@gmail.com>
2023-05-16 17:55:27 +0700

    fix bug update

--- Controllers/User.controller.js
+++ Controllers/User.controller.js
@@ -82,39 +82,25 @@ async function getPlayerById(req, res, next) {
 async function updateUser(req, res, next) {
   try {
     const { id } = req.params;
-    const findUser = await prisma.user.findUnique({ where: { id } });
-
-    if (!findUser)
-      return res
-        .status(404)
-        .json({ result: "not found", message: `Player with ${id} not found` });
-
     const { Email, Username, Password, Total_score, Biodata, City } = req.body;
 
-    if (findUser) {
-      const updateData = await prisma.user.update({
-        where: { id },
-        data: {
-          Email,
-          Username,
-          Password,
-          Total_score,
-          Biodata,
-          City,
-        },
-      });
+    const updateData = await prisma.user.update({
+      where: { id },
+      data: {
+        Email,
+        Username,
+        Password,
+        Total_score,
+        Biodata,
+        City,
+      },
+    });
 
-      res.status(200).json({
-        result: "Success",
-        message: `User with id = ${id} berhasil di update`,
-        data: updateData,
-      });
-    } else {
-      res.status(500).json({
-        result: "Error",
-        message: "Internal Server Error",
-      });
-    }
+    res.status(200).json({
+      result: "Success",
+      message: `User with id = ${id} berhasil di update`,
+      data: updateData,
+    });
   } catch (error) {
     console.log(error);
   }
