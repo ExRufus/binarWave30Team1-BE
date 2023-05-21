5aba34e5ccb80dfe825cbc7536563c058b816651
Adanpradana <adanizkahariz@gmail.com>
2023-05-21 23:05:16 +0700

    try cors config

---
+++ middlewares/cors.js
@@ -0,0 +1,16 @@
+const handleCors = (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With"
+  );
+
+  // intercept OPTIONS method
+  if ("OPTIONS" == req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
+};
+module.exports = handleCors;
