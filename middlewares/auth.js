225730d10daaef0c3147e66fd89c9d201c95fd99
Adanpradana <adanizkahariz@gmail.com>
2023-05-21 16:11:37 +0700

    test

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,17 +2,20 @@ const jwt = require("jsonwebtoken");
 
 const authOnly = (req, res, next) => {
   const authHeader = req.headers.authorization;
-  if (authHeader) {
-    const token = authHeader.split(" ")[1];
-    jwt.verify(token, process.env.TOKEN, (err, user) => {
-      if (err) {
-        return res.status(403).json({ auth: false, message: "forbidden" });
-      }
-      req.user = user;
-      next();
-    });
-  } else {
-    return res.status(401).json({ auth: false, message: "Unauthorized" });
+  try {
+    if (authHeader) {
+      const token = authHeader.split(" ")[1];
+      jwt.verify(token, process.env.TOKEN, (err, user) => {
+        if (err) {
+          return res.status(403).json({ auth: false, message: "forbidden" });
+        }
+        req.user = user;
+        next();
+      });
+    }
+    res.status(401).json({ auth: false, message: "Unauthorized" });
+  } catch (error) {
+    res.json({ msg: error.message });
   }
 };
 
