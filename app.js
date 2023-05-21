229bb37f81cffe524eeaa39b175d18524d0d08a8
Adanpradana <adanizkahariz@gmail.com>
2023-05-21 23:05:04 +0700

    try cors config

--- app.js
+++ app.js
@@ -5,19 +5,14 @@ const cookieParser = require("cookie-parser");
 const routes = require("./Routes");
 const app = express();
 const cors = require("cors");
+const handleCors = require("./middlewares/cors");
 require("dotenv").config();
 const PORT = process.env.PORT;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("./views"));
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST", "DELETE", "UPDATE"],
-    allowedHeaders: ["Authorization"],
-  })
-);
+app.use(handleCors);
 app.use(express.static("public"));
 app.use(flash());
 app.get("/", (req, res) => {
