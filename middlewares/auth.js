72f31c2a4325291c78dd8ab98bdc53db8ba7aa84
Adanpradana <adanizkahariz@gmail.com>
2023-05-21 21:04:01 +0700

    remove trycatch

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,21 +2,17 @@ const jwt = require("jsonwebtoken");
 
 const authOnly = (req, res, next) => {
   const authHeader = req.headers.authorization;
-  try {
-    if (authHeader) {
-      const token = authHeader.split(" ")[1];
-      jwt.verify(token, process.env.TOKEN, (err, user) => {
-        if (err) {
-          return res.status(403).json({ auth: false, message: "forbidden" });
-        }
-        req.user = user;
-        next();
-      });
-    }
-    res.status(401).json({ auth: false, message: "Unauthorized" });
-  } catch (error) {
-    res.json({ msg: error.message });
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.TOKEN, (err, user) => {
+      if (err) {
+        return res.status(403).json({ auth: false, message: "forbidden" });
+      }
+      req.user = user;
+      next();
+    });
   }
+  res.status(401).json({ auth: false, message: "Unauthorized" });
 };
 
 module.exports = authOnly;
