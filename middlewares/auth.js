6942bfe5c2abf6d223a95025ecfa6057cd54fe34
Adanpradana <adanizkahariz@gmail.com>
2023-05-21 21:51:18 +0700

    fix bug

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,13 +4,19 @@ const authOnly = (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (authHeader) {
     const token = authHeader.split(" ")[1];
-    jwt.verify(token, process.env.TOKEN, (err, user) => {
-      if (err) {
-        return res.status(403).json({ auth: false, message: "forbidden" });
-      }
-      req.user = user;
-      next();
-    });
+    try {
+      jwt.verify(token, process.env.TOKEN, (err, user) => {
+        if (err) {
+          return res.status(403).json({ auth: false, message: "forbidden" });
+        }
+        req.user = user;
+        next();
+      });
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ auth: false, message: "Internal server error" });
+    }
   } else {
     return res.status(401).json({ auth: false, message: "Unauthorized" });
   }
