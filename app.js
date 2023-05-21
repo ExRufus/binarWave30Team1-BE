f9bc16a9ef1991587ddc3f2fd4e38b4e869e6b9c
Adanpradana <adanizkahariz@gmail.com>
2023-05-21 17:27:50 +0700

    store images

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.static("./views"));
 app.use(cookieParser());
 app.use(cors());
-
+app.use(express.static("public"));
 app.use(flash());
 app.get("/", (req, res) => {
   res.json({ message: "api running !!" });
