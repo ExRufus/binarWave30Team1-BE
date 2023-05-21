3e3c5b4a6af462e88d4aad3f8cfed6dc898d0cad
Adanpradana <adanizkahariz@gmail.com>
2023-05-21 21:56:00 +0700

    test

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,19 +4,13 @@ const authOnly = (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (authHeader) {
     const token = authHeader.split(" ")[1];
-    try {
-      jwt.verify(token, process.env.TOKEN, (err, user) => {
-        if (err) {
-          return res.status(403).json({ auth: false, message: "forbidden" });
-        }
-        req.user = user;
-        next();
-      });
-    } catch (error) {
-      return res
-        .status(500)
-        .json({ auth: false, message: "Internal server error" });
-    }
+    jwt.verify(token, process.env.TOKEN, (err, user) => {
+      if (err) {
+        return res.status(403).json({ auth: false, message: "forbidden" });
+      }
+      req.user = user;
+      next();
+    });
   } else {
     return res.status(401).json({ auth: false, message: "Unauthorized" });
   }
