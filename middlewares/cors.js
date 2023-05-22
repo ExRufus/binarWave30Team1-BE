624c6eadb17e001024c353f288be75818e82715a
Adanpradana <adanizkahariz@gmail.com>
2023-05-22 19:09:08 +0700

    cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,11 +1,6 @@
 const handleCors = (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, Content-Length, X-Requested-With, application/json"
-  );
-
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
   // intercept OPTIONS method
   if ("OPTIONS" == req.method) {
     res.send(200);
