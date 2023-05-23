149c57056e26effe90576c47bc1e859abc97a7f2
Adanpradana <adanizkahariz@gmail.com>
2023-05-23 07:36:41 +0700

    change cors again

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,7 +1,10 @@
 const handleCors = (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type,token"); // intercept OPTIONS method
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type,token, authorization"
+  ); // intercept OPTIONS method
   if ("OPTIONS" == req.method) {
     res.send(200);
   } else {
